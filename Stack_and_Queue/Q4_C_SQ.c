c2469b20f1f28f6603bbddf28668ba35d8e991fa
smart-cau <1995hyunwoo@naver.com>
2024-07-30 10:08:06 +0000

    solve Stack & Queue 4번

--- Stack_and_Queue/Q4_C_SQ.c
+++ Stack_and_Queue/Q4_C_SQ.c
@@ -110,9 +110,24 @@ int main()
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-void reverse(Queue *q)
-{
-/* add your code here */
+void reverse(Queue *q) {
+	/* add your code here */
+	Stack s;
+	s.ll.head = NULL;
+	s.ll.size = 0;
+	s.ll.tail = NULL;
+
+	int item;	
+	while (!isEmptyQueue(q)) {		
+		item = dequeue(q);
+		push(&s, item);
+	}
+		
+	while (!isEmptyStack(&s)) {
+		item = pop(&s);
+		enqueue(q, item);
+	}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
